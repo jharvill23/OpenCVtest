82778b47daed636706d2395d452d61037aed59e1
John <jharvill23drummer@gmail.com>
2018-05-25 13:17:09 -0500

    third commit for practice

--- app/src/main/java/com/example/jbh150030/opencvtest/MainActivity.java
+++ app/src/main/java/com/example/jbh150030/opencvtest/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         }
     //some changes
     }
-
+    //more changes for third commit
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
