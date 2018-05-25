008c25fec8f4d763ac3affb5830aa2d843177f4d
John <jharvill23drummer@gmail.com>
2018-05-25 10:54:51 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jbh150030/opencvtest/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.jbh150030.opencvtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG="MainActivity"; ///Type this into string for filter to check that opencv actually loaded
+
+    static {
+        if(OpenCVLoader.initDebug()){
+            Log.d(TAG, "Opencv successfully loaded");
+        }
+        else{
+            Log.d(TAG, "Opencv not loaded");
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
